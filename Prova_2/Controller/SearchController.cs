e68c217de91bdcd4e78c999b94909c305661ff2d
Natan Martins Rodrigues <natan.m.rodrigues@academico.unirv.edu.br>
2024-05-05 17:58:55 -0300

    Finalizando projeto

--- Prova_2/Controller/SearchController.cs
+++ Prova_2/Controller/SearchController.cs
@@ -23,20 +23,24 @@ public class SearchController : ControllerBase
                                              [FromHeader] string education, [FromHeader] string income, [FromHeader] int vehicleYear,
                                              [FromHeader] string vehicleType, [FromHeader] string annualMileage)
     {
-        PeopleData peopleData = new PeopleData(age, gender, drivingExperience, education, income, vehicleYear, vehicleType, annualMileage);
-        string ageGroup = peopleData.GetAgeGroup(age);
-        string drivingExperienceGroup = peopleData.GetDrivingExperienceGroup(drivingExperience);
-        string vehicleYearGroup = peopleData.GetVehicleYearGroup(vehicleYear);
-        string annualMileageGroup = peopleData.GetAnnualMileageGroup(annualMileage);
+        try 
+        {        
+            PeopleData peopleData = new PeopleData(age, gender, drivingExperience, education, income, vehicleYear, vehicleType, annualMileage);
+            string ageGroup = peopleData.GetAgeGroup(age);
+            string drivingExperienceGroup = peopleData.GetDrivingExperienceGroup(drivingExperience);
+            string vehicleYearGroup = peopleData.GetVehicleYearGroup(vehicleYear);
+            string annualMileageGroup = peopleData.GetAnnualMileageGroup(annualMileage);
 
-        List<DriverData> driverData = _dataLoad.Search();
-        var objDriverData = driverData.Where(d => d.AGE == ageGroup && d.GENDER == gender && d.DRIVING_EXPERIENCE == drivingExperienceGroup 
-                                             && d.EDUCATION == education && d.INCOME == income && d.VEHICLE_YEAR == vehicleYearGroup 
-                                             && d.VEHICLE_TYPE == vehicleType && d.ANNUAL_MILEAGE == annualMileageGroup);
-        var creditScore = objDriverData.Select(d => d.CREDIT_SCORE);
-
-        Response.Headers.Add("accept", "*/*");
-        
-        return creditScore; 
+            List<DriverData> driverData = _dataLoad.Search();
+            var objDriverData = driverData.Where(d => d.AGE == ageGroup && d.GENDER == gender && d.DRIVING_EXPERIENCE == drivingExperienceGroup 
+                                                && d.EDUCATION == education && d.INCOME == income && d.VEHICLE_YEAR == vehicleYearGroup 
+                                                && d.VEHICLE_TYPE == vehicleType && d.ANNUAL_MILEAGE == annualMileageGroup);
+            var creditScore = objDriverData.Select(d => d.CREDIT_SCORE);
+            
+            return creditScore; 
+        }
+        catch (System.Exception ex) {
+            return BadRequest("Chamou o endpoint mas deu erro: " + Constants.CAMINHO_ARQUIVO + ex.Message + ex.StackTrace );
+        }
     }
 }
